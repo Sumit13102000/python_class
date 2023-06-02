003cbf75c77058fd979ef29a0dbbd2fd3e65adcf
Sumit13102000 <sumeetkumarpal420@gmail.com>
2023-06-02 12:54:55 +0530

    conditions.py file was updated

--- condition.py
+++ condition.py
@@ -1,6 +1,10 @@
+# This is a code to find a number is even or odd
+
 n = int(input())
 if n%2==0:
-    print(f"(n) is a even number")
+    print(f"{n} is a even number")
 else:
-    print(f"(n) is a odd number")
+    print(f"{n} is a odd number")
+
+# 
 
